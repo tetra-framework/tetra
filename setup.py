9c95f9fb1ff050d24e909a5e67ab2ec9388748fb
Sam Willis <sam.willis@gmail.com>
2022-06-13 20:37:05 +0100

    Support for Python 3.7+ and Django 3.2+

--- setup.py
+++ setup.py
@@ -19,11 +19,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
     install_requires=[
         'cryptography>=37.0.1',
-        'Django>=4.0.3',
+        'Django>=3.2.0',
         'python-dateutil>=2.8.2',
-        'sourcetypes>=0.0.3',
+        'sourcetypes>=0.0.4',
     ]
 )
