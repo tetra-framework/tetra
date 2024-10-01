420efdc2618bdc53291c608185bbd3c766b775c7
Christian González <christian.gonzalez@nerdocs.at>
2024-10-01 18:54:50 +0200

    bump version to 0.1.2

--- tetra/__init__.py
+++ tetra/__init__.py
@@ -1,6 +1,7 @@
 """Full stack component framework for Django using Alpine.js"""
+
 from .components import BasicComponent, Component, public
 from .library import Library
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __version_info__ = tuple([int(num) for num in __version__.split(".")])
