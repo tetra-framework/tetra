8a30d1f7a43d2e23a58a23a0ee17511f48d50656
Christian González <christian.gonzalez@nerdocs.at>
2025-03-26 08:14:56 +0100

    add attrs merging test

--- tests/test_component_attrs.py
+++ tests/test_component_attrs.py
@@ -2,7 +2,7 @@ from tests.conftest import extract_component
 from tests.main.helpers import render_component_tag
 
 
-def test_basic_component(request):
+def test_attrs(request):
     """Tests a simple component with / end"""
     content = render_component_tag(
         request, "{% @ main.default.SimpleComponentWithAttrs / %}"
@@ -11,4 +11,16 @@ def test_basic_component(request):
     assert (
         extract_component(content, innerHTML=False)
         == '<div class="class1" id="component">content</div>'
+    )
+
+
+def test_attrs_merge(request):
+    """Tests a simple component with / end"""
+    content = render_component_tag(
+        request, "{% @ main.default.SimpleComponentWithAttrs attrs: class='class2' / %}"
+    )
+    assert extract_component(content) == "content"
+    assert (
+        extract_component(content, innerHTML=False)
+        == '<div class="class2 class1" id="component">content</div>'
     )
