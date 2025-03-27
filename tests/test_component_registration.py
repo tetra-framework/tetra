3645a2901687c01391b1442823ba87f8fab80812
Christian González <christian.gonzalez@nerdocs.at>
2025-03-27 13:08:38 +0100

    fix tests: rename local TestComponents, so that pytest doesn't expect them to be TestCases

--- tests/test_component_registration.py
+++ tests/test_component_registration.py
@@ -2,7 +2,7 @@ from tetra import Library, BasicComponent
 
 
 # TestComponent will be registered manually
-class TestComponent1(BasicComponent):
+class Component1(BasicComponent):
     template = """<div></div>"""
 
 
@@ -10,19 +10,19 @@ lib2 = Library("lib2", app="main")
 
 
 @lib2.register
-class TestComponent2(BasicComponent):
+class Component2(BasicComponent):
     template = """<div></div>"""
 
 
 def test_register_component_manually(current_app):
     """create a lib and register a component manually and make sure it exists in the library"""
     lib1 = Library("lib1", current_app)
-    lib1.register(TestComponent1)
-    assert lib1.components["test_component1"] is TestComponent1
+    lib1.register(Component1)
+    assert lib1.components["component1"] is Component1
 
 
 def test_register_decorator(current_app):
     """get a registered library with  a component registered there using the decorator
     syntax and make sure it exists in the library"""
     lib2 = Library("lib2", current_app)
-    assert lib2.components["test_component2"] is TestComponent2
+    assert lib2.components["component2"] is Component2
