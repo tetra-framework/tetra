816cc6b0ca58be9291e5d7a1ebd9def07796f2c0
Christian González <christian.gonzalez@nerdocs.at>
2025-05-12 11:45:33 +0200

    remove unnecessary JavaScript semicolons

--- tetra/templates/script.js
+++ tetra/templates/script.js
@@ -1,8 +1,8 @@
 (() => {
-  let __script = {{ component_script|safe }};
-  let __serverMethods = {{ component_server_methods|safe }};
-  let __serverProperties = {{ component_server_properties|safe }};
-  let __componentName = '{{ component_name|safe }}';
+  let __script = {{ component_script|safe }}
+  let __serverMethods = {{ component_server_methods|safe }}
+  let __serverProperties = {{ component_server_properties|safe }}
+  let __componentName = '{{ component_name|safe }}'
   window.document.addEventListener('alpine:init', () => {
     Tetra.makeAlpineComponent(
       __componentName,
