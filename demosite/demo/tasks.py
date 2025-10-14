2df4d37e020c6a68a215dfa1fd99ef7556f39b75
Christian González <christian.gonzalez@nerdocs.at>
2025-10-14 20:51:32 +0200

    fix loading models before apps are loaded

--- demosite/demo/tasks.py
+++ demosite/demo/tasks.py
@@ -1,11 +1,11 @@
 import asyncio
 import random
 
-from demo.models import BreakingNews
 from tetra.dispatcher import ComponentDispatcher
 
 
 async def send_breaking_news_to_channel():
+    from demo.models import BreakingNews
     while True:
         count = await BreakingNews.objects.acount()
         offset = random.randint(0, count - 1)
