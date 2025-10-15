2d70c040522ede52cb9a68fb47e7145c5fd14b0a
Christian González <christian.gonzalez@nerdocs.at>
2025-10-15 08:01:34 +0200

    bump version to 0.5.1

--- tetra/__init__.py
+++ tetra/__init__.py
@@ -4,5 +4,5 @@ from .components import BasicComponent, Component, ReactiveComponent, public
 from .library import Library
 
 __all__ = [BasicComponent, Component, ReactiveComponent, public, Library]
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 __version_info__ = tuple([int(num) for num in __version__.split(".")])
