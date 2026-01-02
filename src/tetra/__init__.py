b9551ca0e24a109a1a2621447205281c62740afd
Christian González <christian.gonzalez@nerdocs.at>
2026-01-02 10:09:45 +0100

    bump version to 0.6.0

--- src/tetra/__init__.py
+++ src/tetra/__init__.py
@@ -15,5 +15,5 @@ except ImportError:
     pass
 
 __all__ += [BasicComponent, Component, public, Library]
-__version__ = "0.5.3"
+__version__ = "0.6.0"
 __version_info__ = tuple([int(num) for num in __version__.split(".")])
