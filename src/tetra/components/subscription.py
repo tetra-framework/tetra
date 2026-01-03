30fc79c7b6bcf5a07ead7fb522d7365d5ce5eb78
Christian González <christian.gonzalez@nerdocs.at>
2026-01-03 23:16:14 +0100

    add type checking annotation correction

--- src/tetra/components/subscription.py
+++ src/tetra/components/subscription.py
@@ -1,2 +1,7 @@
 # TODO evaluate moving to dispatcher
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from tetra import ReactiveComponent
+
 registry: dict[str, list["ReactiveComponent"]] = {}
