ed7b43f3c0cd93c3c10b98ac48a43785e5806e9d
Christian González <christian.gonzalez@nerdocs.at>
2026-01-04 02:08:39 +0100

    allow alphanumeric version info (1.0.3a1,...)

--- src/tetra/__init__.py
+++ src/tetra/__init__.py
@@ -16,4 +16,4 @@ except ImportError:
 
 __all__ += [BasicComponent, Component, public, Library]
 __version__ = "0.6.3"
-__version_info__ = tuple([int(num) for num in __version__.split(".")])
+__version_info__ = tuple([num for num in __version__.split(".")])
