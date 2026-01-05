b2c88b1f37a959b7e4dcc97add291e274d96f432
Christian González <christian.gonzalez@nerdocs.at>
2026-01-05 17:28:51 +0100

    simplify has_reactive_components implementation by removing property decorators
    This fixes a nasty bug that caused "if has_reactive_components" always to be true, because of the nature of properties.

--- src/tetra/globals.py
+++ src/tetra/globals.py
@@ -1,12 +1,2 @@
 # This is set to True if any ReactiveComponent is initialized
-_has_reactive_components: bool = False
-
-
-@property
-def has_reactive_components():
-    return _has_reactive_components
-
-
-@has_reactive_components.setter
-def has_reactive_components(value: bool):
-    _has_reactive_components = value
+has_reactive_components: bool = False
