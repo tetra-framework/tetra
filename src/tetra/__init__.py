e49e925ed4c875fd63b677332ffa59d3a89a1a06
Christian González <christian.gonzalez@nerdocs.at>
2026-03-20 16:25:55 +0100

    Bump version to 0.9.3

--- src/tetra/__init__.py
+++ src/tetra/__init__.py
@@ -33,5 +33,5 @@ def __getattr__(name):
     raise AttributeError(f"module {__name__} has no attribute {name}")
 
 
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 __version_info__ = tuple([num for num in __version__.split(".")])
